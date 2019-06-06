8d38276add9bebe91c56e0665bac89796580764d
Nicxes <nicxesh@gmail.com>
2019-06-06 20:33:48 -0300

    Adding menu animation for mobile users

--- components/header.js
+++ components/header.js
@@ -1,29 +1,80 @@
 import Logo from './logo'
 
-function Header() {
-  return (
-    <header className="header container">
+export default class Header extends React.Component {
+  constructor() {
+    super()
 
-      <div>
-        <a href="/">
-          <Logo size="40"/>
-        </a>
-      </div>
+    this.state = {
+      open: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    this.setState({open: !this.state.open})
+  }
+  render() {
+    return (
+      <header className="header container">
 
-      <div>
-        
-      </div>
+        <div className="brand">
+          <a href="/">
+            <Logo size="40"/>
+          </a>
+        </div>
 
-      <style jsx>{`
-        .header {
-          display: grid;
-          grid-template-columns: 50% 50%;
+        <div className="userbox">
+          <span className={this.state.open ? "menu open": "menu"} onClick={ this.handleClick }>
+            <span className="line line-top"></span>
+            <span className="line line-mid"></span>
+            <span className="line line-bot"></span>
+          </span>
+        </div>
 
-          margin: 50px auto;
-        }
-      `}</style>
-    </header>
-  )
+        <style jsx>{`
+          .header {
+            display: grid;
+            grid-template-columns: 50% 50%;
+            align-items: center;
+
+            margin: 50px auto;
+          }
+          .header .userbox {justify-self: end;}
+          .header .menu {
+            cursor: pointer;
+            height: 32px;
+            width: 32px;
+            
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+          .header .menu .line {
+            height: 2px;
+            width: 20px;
+            
+            background: #ccc;
+            border-radius: 3px;
+            transition: all 0.1s ease;
+          }
+          .header .menu:hover .line {background: #fff;}
+          .header .menu .line-mid {margin: 5px 0;}
+
+          .header .menu .line-top {
+            transform: ${this.state.open ? "translateY(2px) rotate(48deg)": "none"}
+          }
+          .header .menu .line-mid {
+            display: ${this.state.open ? "none": "block"}
+          }
+          .header .menu .line-bot {
+            transform: ${this.state.open ? "rotate(-45deg)": "none"}
+          }
+
+          @media only screen and (min-width: 769px) {
+            .header .menu {display: none;}
+          }
+        `}</style>
+      </header>
+    )
+  }
 }
-
-export default Header;
