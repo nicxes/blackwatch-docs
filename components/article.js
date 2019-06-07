2e010daace66480165e8df103159d5df27f90f84
Nicxes <nicxesh@gmail.com>
2019-06-06 22:18:41 -0300

    Making MDX into a article content

--- components/article.js
+++ components/article.js
@@ -1,5 +1,12 @@
 import MDX from '../mdx/hello.mdx'
 
 export default function Article() {
-  return <MDX/>
+  return (
+    <article className="article">
+      <MDX/>
+      <style global jsx>{`
+        .article p a {border-bottom: 1px solid #444;}
+      `}</style>
+    </article>
+  )
 }
